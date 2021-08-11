b32fc8c4de04d5f4b714c0ee362287c16ca7a106
Anna Efimenko <efimenko_av@ayax.ru>
2021-08-11 10:23:46 +0300

    1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,8 +34,9 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    //let data = {goods: [], order: {}};
+    let data = {client, goods: [], order: {}};
+    
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
@@ -43,10 +44,11 @@ function sendRequest(name, phone, address, goods, sum) {
     }
 
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
+    //data.order.sum = name + phone + address + goods + sum;
+    data.order.sum = sum;
+    //data.client = 'Иван';
+    data.client = name + phone;
+    data.goods = title + countOfGoods;
     let jsonData = JSON.stringify(data);
 
     return jsonData;
