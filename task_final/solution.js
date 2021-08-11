d9c0feace3e70fb7b91d5f85de2fa98812995c83
Anna Efimenko <efimenko_av@ayax.ru>
2021-08-11 14:18:18 +0300

    17

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client, order: {}, goods: []};
+    let data = {goods: [], order: {}};
 
     let countOfGoods = goods.length;
 
