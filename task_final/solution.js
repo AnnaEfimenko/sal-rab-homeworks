1657329fe2f46caf0a08407c2424f0b2606d5f20
Anna Efimenko <efimenko_av@ayax.ru>
2021-08-12 08:30:45 +0300

    1

--- task_final/solution.js
+++ task_final/solution.js
@@ -44,10 +44,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = `ул. ` +address["street"] + `, дом ` + address["house"] + `, `+ address["entrance"] + ` подъезд, ` + address["floor"] + ` этаж, кв `+ address["flat"];
-    //data.order.address = `ул. &{address["street"]}, дом &{address["house"]}, &{address["entrance"]} подъезд, &{address["floor"]} этаж, кв &{address["flat"]}`;
+    //data.order.address = `ул. ` +address["street"] + `, дом ` + address["house"] + `, `+ address["entrance"] + ` подъезд, ` + address["floor"] + ` этаж, кв `+ address["flat"];
+    data.order.address = `ул. ${address["street"]}, дом ${address["house"]}, ${address["entrance"]} подъезд, ${address["floor"]} этаж, кв ${address["flat"]}`;
     data.order.sum = sum;
-    data.client = name + ` ` + phone;
+    data.client = `${name}  ${phone}`;
     
     //data.goods = [data.goods[title],data.goods[count]];
     //data.goods = 
