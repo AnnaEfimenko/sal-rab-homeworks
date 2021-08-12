994d5b702ad2dde576bd75bfd58d5b03561a7082
Anna Efimenko <efimenko_av@ayax.ru>
2021-08-12 08:33:00 +0300

    Финальное решение

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,18 +39,13 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        
-        //data.goods.push(goods[i]);
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    //data.order.address = `ул. ` +address["street"] + `, дом ` + address["house"] + `, `+ address["entrance"] + ` подъезд, ` + address["floor"] + ` этаж, кв `+ address["flat"];
     data.order.address = `ул. ${address["street"]}, дом ${address["house"]}, ${address["entrance"]} подъезд, ${address["floor"]} этаж, кв ${address["flat"]}`;
     data.order.sum = sum;
     data.client = `${name} ${phone}`;
     
-    //data.goods = [data.goods[title],data.goods[count]];
-    //data.goods = 
     let jsonData = JSON.stringify({data});
 
     return jsonData;
